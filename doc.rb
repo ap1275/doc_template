59f5fc78b26b282491370eaa03e3f4a757dc4f33
0xdead6eef <fake@mail.com>
2018-04-25 12:49:45 +0900

    init

---
+++ doc.rb
@@ -0,0 +1,34 @@
+#!usr/bin/env ruby
+
+require './.doc_helper'
+
+################################################################################
+title = ""
+
+created = ""
+
+author = [
+]
+
+tag = [
+]
+
+media = [
+]
+
+ref = [
+]
+
+quota = [
+]
+
+abstract =
+<<EOS
+EOS
+
+body =
+<<EOS
+EOS
+################################################################################
+
+output title, created, author, tag, media, ref, quota, abstract, body
